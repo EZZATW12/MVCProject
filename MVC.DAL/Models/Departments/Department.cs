f48b68913708bad670fb7d10c95677e979a6b083
ezzat <ezzatw122@gmail.com>
2025-02-04 04:03:36 +0200

    Creating Department and it's Configrations

--- MVC.DAL/Models/Departments/Department.cs
+++ MVC.DAL/Models/Departments/Department.cs
@@ -4,13 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MVC.DAL.Models.Department
+namespace MVC.DAL.Models.Departments
 {
-    internal class Department : ModelBase
+    public class Department : ModelBase
     {
         public string Name { get; set; } =null!;
         public string Code { get; set; } = null!;
-        public string Description { get; set; } = null!;
+        public string? Description { get; set; } 
         DateOnly CreationDate { get; set; }
     }
 }
---
+++ MVC.DAL/persistance/Data/DbContextSeed.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC.DAL.persistance.Data
+{
+    internal class DbContextSeed
+    {
+        public static void Seed(ApplicationDbContext context)
+        {
+            // to do
+        }
+    }
+}
